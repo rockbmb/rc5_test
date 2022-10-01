7de0031f0db0fc2bf05aeed479f68311c9d1c1eb
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2022-10-01 03:24:34 +0100

    Implement key table generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,37 @@
-use std::{ops::{Mul, Add, BitOr, BitXor, BitAnd, Sub, Shl, Shr}};
-use num::One;
-
-enum SupportedRC5WordType {
-	Word16,
-	Word32,
-	Word64
-}
-
-impl SupportedRC5WordType {
-	fn get_size_in_bits(&self) -> u32 {
-		match self {
-			Self::Word16 => u16::BITS,
-			Self::Word32 => u32::BITS,
-			Self::Word64 => u64::BITS
-		}
-	}
-}
+use std::{ops::{BitOr, BitXor, BitAnd, Shl, Shr}, cmp::max};
+use num::{One, Integer};
+use num_traits::ops::wrapping;
+use num_integer::div_ceil;
 
 type WordSize = u32;
 
 type NumberOfRounds = u32;
 
-type KeySize = u32;
+/**
+ * Maximum allowable number of rounds, by default the paper advised it to be 255.
+ */
+pub const MAX_ALLOWABLE_ROUNDS : NumberOfRounds = 255;
+
+type KeyLength = u32;
+
+/**
+ * Maximum allowable size of key in bytes, by default the paper advised it to be 255.
+ */
+pub const MAX_ALLOWABLE_KEY_LENGTH : KeyLength = 255;
 
 type KeyTableSize = u32;
 
 type BlockLength = u32;
 
-trait RC5Word
+#[derive(Debug)]
+pub enum RC5Error {
+	InvalidNumberOfRounds,
+	InvalidKeyLength,
+	InvalidLVectorLen,
+	InvalidSVectorLen
+}
+
+pub trait RC5Word
 {
 	type T;
 
@@ -38,16 +42,13 @@ trait RC5Word
 	fn Q() -> Self::T;
 
 	fn rotl(x : Self::T, y : Self::T) -> Self::T
-	where Self::T : Add<Output = Self::T> +
-		  Mul<Output = Self::T> +
+	where Self::T : Integer +
 		  BitOr<Output = Self::T> +
 		  BitXor<Output = Self::T> +
 		  BitAnd<Output = Self::T> +
 		  Shl<Output = Self::T> +
 		  Shr<Output = Self::T> +
-		  Sub<Output = Self::T> +
 		  From<u32> +
-		  One +
 		  Copy
 	{
 		let w : Self::T = Self::T::from(Self::get_size_in_bits());
@@ -57,16 +58,13 @@ trait RC5Word
 	}
 
 	fn rotr(x : Self::T, y : Self::T) -> Self::T
-	where Self::T : Add<Output = Self::T> +
-		  Mul<Output = Self::T> +
+	where Self::T : Integer +
 		  BitOr<Output = Self::T> +
 		  BitXor<Output = Self::T> +
 		  BitAnd<Output = Self::T> +
 		  Shl<Output = Self::T> +
 		  Shr<Output = Self::T> +
-		  Sub<Output = Self::T> +
 		  From<u32> +
-		  One +
 		  Copy
 	{
 		let w : Self::T = Self::T::from(Self::get_size_in_bits());
@@ -124,17 +122,16 @@ impl RC5Word for u64 {
 	}
 }
 
-/**
- * Structure that represents an RC5 configuration to be used to encrypt/decrypt data
- * with a certain private key.
- * 
- * They key is not part of the struct, it is meant to be passed as a parameter to the
- * relevant methods - this means that the symbol table S is recomputed every invocation.
- */
-struct RC5 {
+/// Structure that represents an RC5 configuration to be used to encrypt/decrypt data
+/// with a certain private key.
+///
+/// They key is not part of the struct, it is meant to be passed as a parameter to the
+/// relevant methods - this means that the symbol table S is recomputed every invocation.
+#[derive(Debug)]
+pub struct RC5 {
 	word_size : WordSize,
 	rounds : NumberOfRounds,
-	key_size : KeySize,
+	key_size : KeyLength,
 
 	key_table_size : KeyTableSize,
 
@@ -144,25 +141,85 @@ struct RC5 {
 }
 
 impl RC5 {
-	fn create_rc5<T : RC5Word>(rounds : NumberOfRounds, key_size : KeySize) -> RC5 {
-		let w : WordSize = T::get_size_in_bits();
+	pub fn create_rc5<T : RC5Word>(rounds : NumberOfRounds, key_size : KeyLength) -> Result<RC5, RC5Error> {
 
-		RC5 {
+		if rounds < 0 || rounds > MAX_ALLOWABLE_ROUNDS {
+			return Err(RC5Error::InvalidNumberOfRounds);
+		} else if key_size < 0 || key_size > MAX_ALLOWABLE_KEY_LENGTH {
+			return Err(RC5Error::InvalidKeyLength);
+		}
+
+		let w : WordSize = T::get_size_in_bits();
+		Ok(RC5 {
 			word_size : w,
 			rounds,
 			key_size,
 			key_table_size : 2 * rounds + 1,
 			block_length : 2 * w
+		})
+	}
+
+	pub fn setup_rc5<W>(&self, key: Vec<u8>) -> Result<Vec<W>, RC5Error>
+	where W : Copy +
+			  RC5Word<T = W> +
+			  Integer +
+			  From<u32> +
+			  Shl<Output = W> +
+			  BitOr<Output = W> +
+			  BitAnd<Output = W> +
+			  BitXor<Output = W> +
+			  Shr<Output = W> {
+		let c : usize =  max(1, div_ceil(8 * self.key_size, self.word_size)) as usize;
+		let u : usize = self.word_size as usize / 8;
+		let mut L  : Vec<W> = vec![W::from(0); c];
+		let t : usize = 2 * (self.rounds as usize) + 1;
+		let mut S : Vec<W> = vec![W::from(0); t];
+
+		L[c-1] = W::from(0);
+		for i in (0 .. self.key_size as usize - 1).rev() {
+			L[i / u] = L[i/u] << W::from(8) + W::from(key[i].into());
 		}
+		if L.len() != c {
+			return Err(RC5Error::InvalidLVectorLen);
+		}
+
+		S[0] = W::P();
+		for i in 1 .. t-1 {
+			S[i] = S[i-1] + W::Q();
+		}
+		if L.len() != t {
+			return Err(RC5Error::InvalidSVectorLen);
+		}
+
+		let mut A : W = W::from(0);
+		let mut B : W = W::from(0);
+
+		let mut i : usize = 0;
+		let mut j : usize = 0;
+		let mut k : usize = 0;
+
+		while k < 3 * max(t, c) {
+			S[i] = W::rotl(S[i] + A + B, W::from(3));
+			A = S[i];
+
+			L[j] = W::rotl(L[j] + A + B, A + B);
+			B = L[j];
+
+			i = (i + 1) % t;
+			j = (j + 1) % c;
+			k += 1;
+		}
+
+		Ok(S)
 	}
 
 }
 
 /*
- * This function should return a cipher text for a given key and plaintext
- *
- */
-fn encode(key: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
+* This function should return a cipher text for a given key and plaintext
+*
+*/
+ fn encode(key: Vec<u8>, plaintext: Vec<u8>) -> Vec<u8> {
 	let mut ciphertext = Vec::new();
 	ciphertext
 }
